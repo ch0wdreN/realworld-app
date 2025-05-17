07346c36a5bf71d8940bfcf94a2fa6bf795f7b3a
ch0wdreN <i19yanagi@gmail.com>
2025-05-18 01:58:36 +0900

    feat: add modules

--- infra/main.tf
+++ infra/main.tf
@@ -1,5 +1,50 @@
+module "repository" {
+  source = "./modules/registry"
+
+  project_id   = var.project
+  project_name = var.project_name
+  region       = var.region
+}
+
+module "cloud_run" {
+  source = "./modules/cloud_run"
+
+  project_id      = var.project
+  region          = var.region
+  service_name    = "app"
+  repository_name = module.repository.repository_name
+  port            = 8080
+  project_number  = var.project_number
+}
+
+module "certificate" {
+  source = "./modules/certificate"
+
+  project_name  = var.project_name
+  lb_sub_domain = "api.${var.base_domain}"
+}
+
+module "load_balancer" {
+  source = "./modules/load_balancer"
+
+  cert_id      = module.certificate.self_link
+  project_name = var.project_name
+  region       = var.region
+  run_service  = module.cloud_run.run_id
+  domain       = "api.${var.base_domain}"
+}
+
+module "cloud_dns" {
+  source = "./modules/cloud_dns"
+
+  lb_sub_domain = "api.${var.base_domain}"
+  zone          = var.dns_zone_name
+  lb_ip         = module.load_balancer.lb_ip
+}
+
 module "vpc" {
   source = "./modules/vpc"
 
   project_name = var.project_name
+  region       = var.region
 }
--- infra/variables.tf
+++ infra/variables.tf
@@ -17,3 +17,15 @@ variable "project_name" {
   description = "Project Name"
   type        = string
 }
+
+variable "project_number" {
+  type = string
+}
+
+variable "base_domain" {
+  type = string
+}
+
+variable "dns_zone_name" {
+  type = string
+}
