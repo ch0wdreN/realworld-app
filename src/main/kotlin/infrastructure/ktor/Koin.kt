544628ff6bb98be93893336fb3106d43b78cc5fc
ch0wdreN <i19yanagi@gmail.com>
2025-06-04 00:55:57 +0900

    refactor: rename configure name

--- src/main/kotlin/infrastructure/ktor/Koin.kt
+++ src/main/kotlin/infrastructure/ktor/Koin.kt
@@ -1,13 +1,13 @@
 package io.ch0wdren.infrastructure.ktor
 
-import infrastructure.ktor.handler.handlerModule
 import io.ch0wdren.application.service.factoryModule
+import io.ch0wdren.infrastructure.ktor.handler.handlerModule
 import io.ktor.server.application.Application
 import io.ktor.server.application.install
 import org.koin.ktor.plugin.Koin
 import org.koin.logger.slf4jLogger
 
-fun Application.configureFrameworks() {
+fun Application.configureKoin() {
   install(Koin) {
     slf4jLogger()
     modules(
--- src/main/kotlin/infrastructure/ktor/Module.kt
+++ src/main/kotlin/infrastructure/ktor/Module.kt
@@ -3,7 +3,7 @@ package io.ch0wdren.infrastructure.ktor
 import io.ktor.server.application.Application
 
 fun Application.module() {
-  configureFrameworks()
+  configureKoin()
   configureSerialization()
   configureSecurity()
   configureSwagger()
