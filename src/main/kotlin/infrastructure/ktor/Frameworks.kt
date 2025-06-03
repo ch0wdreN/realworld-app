00b5c4e924e1d9b4bb377c199399a5065f96b365
ch0wdreN <i19yanagi@gmail.com>
2025-06-03 15:03:06 +0900

    feat: DI

--- src/main/kotlin/infrastructure/ktor/Frameworks.kt
+++ src/main/kotlin/infrastructure/ktor/Frameworks.kt
@@ -1,9 +1,9 @@
 package io.ch0wdren.infrastructure.ktor
 
-import io.ch0wdren.infrastructure.postgresql.ConnectionPool
+import infrastructure.ktor.handler.handlerModule
+import io.ch0wdren.application.service.factoryModule
 import io.ktor.server.application.Application
 import io.ktor.server.application.install
-import org.koin.dsl.module
 import org.koin.ktor.plugin.Koin
 import org.koin.logger.slf4jLogger
 
@@ -11,11 +11,8 @@ fun Application.configureFrameworks() {
   install(Koin) {
     slf4jLogger()
     modules(
-      module {
-        single {
-          ConnectionPool()
-        }
-      },
+      factoryModule,
+      handlerModule,
     )
   }
 }
