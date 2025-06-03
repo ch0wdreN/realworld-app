a405eeb94c8111dbf9caa3b3f391158420a7c273
ch0wdreN <i19yanagi@gmail.com>
2025-06-03 15:03:15 +0900

    feat: routing

--- src/main/kotlin/infrastructure/ktor/Routing.kt
+++ src/main/kotlin/infrastructure/ktor/Routing.kt
@@ -1,35 +1,21 @@
 package io.ch0wdren.infrastructure.ktor
 
-import io.ch0wdren.domain.User
+import io.ch0wdren.infrastructure.ktor.handler.UserHandler
 import io.ktor.server.application.Application
-import io.ktor.server.response.respond
-import io.ktor.server.routing.Route
 import io.ktor.server.routing.get
-import io.ktor.server.routing.put
 import io.ktor.server.routing.route
 import io.ktor.server.routing.routing
+import org.koin.ktor.ext.inject
 
 fun Application.configureRouting() {
+  val userHandler by inject<UserHandler>()
   routing {
     route("/api") {
-      userRoute()
-    }
-  }
-}
-
-fun Route.userRoute() {
-  route("/user") {
-    get {
-      call.respond(
-        User(
-          "hoge@example.com",
-          "hoge",
-          "i am hoge",
-          "https://image.example.com",
-        ),
-      )
-    }
-    put {
+      route("/user") {
+        get {
+          userHandler.getUser(call)
+        }
+      }
     }
   }
 }
